36709424436238650d2dda7fe8abacf411fac2b2
VincentChungHeiPoon <boy19970722@hotmail.com>
2019-12-22 00:24:09 -0600

    improved DataCollection

--- datacollection.py
+++ datacollection.py
@@ -2,32 +2,41 @@
 """
 Created on Thu Dec 19 22:13:39 2019
 
-@author: boy19
+@author: Vincent Poon
 """
 import yfinance as yf
 import pandas as pd
 import os
 import re
 
+class SymbolNamePair:
+    
+    def __init__(self, symbol, name):
+        self.symbol = symbol
+        self.name = name
+        
 
 class DataCollection:
     #list of stock that will be collected
     #read from file
-            
-    def get_stock_list_string():
+    
+    #get parameter for get_stock_data()
+    def get_stock_list_string(filename):
         stock_string = ''
         start_date = ''
         end_date = ''
         try:
-            file = open('TrackingStock.txt', 'r')
+            file = open(filename, 'r')
             print('List of tracking stocks are')
             
             #first line in file is start date
-            start_date = file.readline().strip('\n')
+            #index 1 is element 2 in the splited string
+            #eg start day = 2019-12-10 after split produce ['start day', '2019-12-10']
+            start_date = file.readline().strip('\n').split(' = ')[1]
             #second line in file is end date
-            end_date = file.readline().strip('\n')
+            end_date = file.readline().strip('\n').split(' = ')[1]
             for line in file:
-                stock = line.strip('\n')
+                stock = line.strip('\n').split(' = ')[1]     
                 print(stock)    
                 stock_string = stock_string + ' ' + stock
             file.close()
@@ -35,11 +44,27 @@ class DataCollection:
         except:
             print('Error at setting parameter')
             
-    def get_stock_data():
-        start_date, end_date, stock_list_string = DataCollection.get_stock_list_string()
+    def get_stock_data(filename):
+        start_date, end_date, stock_list_string = DataCollection.get_stock_list_string(filename)
         return yf.download(stock_list_string, start=start_date, end=end_date)        
     
-    def save_data_as_csv():
-        data = DataCollection.get_stock_data()
+    #return list of name of tracking stocks in alias, GOOGL returns google
+    def get_stock_name(filename):
+        pair = []
+        file = open(filename, 'r')
+        #skipping over first 2 line
+        file.readline()
+        file.readline()
+        for line in file:
+            #appending company name
+            split = line.strip('\n').split(' = ')        
+            #position as in .txt
+            pair.append(SymbolNamePair(symbol = split[1], name = split[0]))
+
+        return pair
+    
+    def save_data_as_csv(filename, targetFileName):
+        data = DataCollection.get_stock_data(filename)
+        loc = 'Dataset/' + targetFileName
         #path might be differnt from computer to computer, so we will use OS path to get the right path on each device
-        data.to_csv(path_or_buf  = os.path.join(os.path.dirname(os.path.realpath(__file__)),'Dataset\data.csv'))
+        data.to_csv(path_or_buf  = os.path.join(os.path.dirname(os.path.realpath(__file__)),loc))
---
+++ test.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 21 23:41:27 2019
+
+@author: boy19
+"""
+
+import datacollection
+data = datacollection.DataCollection.get_stock_data()
