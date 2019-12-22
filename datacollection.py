82860fd6a84cffb821afcd9763ae4e73ecc9a87f
VincentChungHeiPoon <boy19970722@hotmail.com>
2019-12-21 20:04:59 -0600

    removed parameter to for DataCollection class

--- datacollection.py
+++ datacollection.py
@@ -7,6 +7,7 @@ Created on Thu Dec 19 22:13:39 2019
 import yfinance as yf
 import pandas as pd
 import os
+import re
 
 
 class DataCollection:
@@ -15,23 +16,30 @@ class DataCollection:
             
     def get_stock_list_string():
         stock_string = ''
+        start_date = ''
+        end_date = ''
         try:
             file = open('TrackingStock.txt', 'r')
             print('List of tracking stocks are')
+            
+            #first line in file is start date
+            start_date = file.readline().strip('\n')
+            #second line in file is end date
+            end_date = file.readline().strip('\n')
             for line in file:
                 stock = line.strip('\n')
                 print(stock)    
                 stock_string = stock_string + ' ' + stock
             file.close()
-            return stock_string
+            return start_date, end_date, stock_string
         except:
-            print('Error at open TrackingStock.txt')
+            print('Error at setting parameter')
             
-    def get_stock_data(start_date, end_date):
-        stock_list_string = DataCollection.get_stock_list_string()
+    def get_stock_data():
+        start_date, end_date, stock_list_string = DataCollection.get_stock_list_string()
         return yf.download(stock_list_string, start=start_date, end=end_date)        
     
-    def save_data_as_csv(start_date, end_date):
-        data = DataCollection.get_stock_data(start_date = start_date, end_date = end_date)
+    def save_data_as_csv():
+        data = DataCollection.get_stock_data()
         #path might be differnt from computer to computer, so we will use OS path to get the right path on each device
         data.to_csv(path_or_buf  = os.path.join(os.path.dirname(os.path.realpath(__file__)),'Dataset\data.csv'))
