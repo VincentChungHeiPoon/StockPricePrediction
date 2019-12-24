a78befd0ac4d9ec93e7a91bbba3395dfe242fb87
VincentChungHeiPoon <boy19970722@hotmail.com>
2019-12-24 00:36:28 -0600

    upgrade utils.py

--- test.py
+++ test.py
@@ -5,5 +5,12 @@ Created on Sat Dec 21 23:41:27 2019
 @author: boy19
 """
 
-import datacollection
-data = datacollection.DataCollection.get_stock_data()
+from utils import DataCollection
+import pandas as pd
+
+
+#data = DataCollection.save_data_as_csv() 
+
+data_df = pd.read_csv('Dataset/Data.csv')
+
+temp = data_df.T.melt(id_vars = ['0'])
